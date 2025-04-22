12721e8c384a57678d596eed03384932d01f7f15
Lennart Landsmeer <lennart@landsmeer.email>
2025-04-22 02:34:05 +0200

    gemini

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ ln --force -s $PWD/vibe ~/.local/bin/vibe
 ln --force -s $PWD/prompt ~/.local/bin/prompt
 ln --force -s $PWD/vibe.sh ~/.local/bin/vibe.sh
 ln --force -s $PWD/tool ~/.local/bin/tool
+ln --force -s $PWD/gemini ~/.local/bin/gemini
